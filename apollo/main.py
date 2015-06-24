244299aebd3bbca76d46b9b6ae10548b8210b144
thyagostall <thstall@gmail.com>
2015-06-23 22:29:37 -0400

    Removed command.py

--- apollo/main.py
+++ apollo/main.py
@@ -1,5 +1,7 @@
-from data import DataManager
-from dbaccess import DataAccess
+import settings
 
-dao = DataAccess('/Users/thyago/proj/apollo/db/baseline.db')
-dmg = DataManager(dao)
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
