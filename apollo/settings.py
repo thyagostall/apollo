7d8a545f73c72952c47b83ccc32be5ce4b6f7c13
thyagostall <thstall@gmail.com>
2015-06-24 01:46:10 -0400

    Added support for load and save settings in json format

--- apollo/settings.py
+++ apollo/settings.py
@@ -1,4 +1,16 @@
+import json
+
 __settings_data = {}
+
+def load(filename):
+    global __settings_data
+
+    with open(filename, 'r') as settings_file:
+        __settings_data = json.load(settings_file)
+
+def save(filename):
+    with open(filename, 'w') as settings_file:
+        json.dump(__settings_data, settings_file)
 
 def get(name):
     return __settings_data[name]
