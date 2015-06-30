09e771c24ff3ef2c29d1c6de14d38cdeab910df2
thyagostall <thstall@gmail.com>
2015-06-29 21:16:51 -0400

    Category system created

---
+++ apollo/category.py
@@ -0,0 +1,37 @@
+class CategoryNotFound(Exception):
+    pass
+
+
+class Category(object):
+    def __init__(self, name, id=None):
+        self.category_id = id
+        self.name = name
+
+
+class CategoryManager(object):
+    def __init__(self, db):
+        self.db = db
+
+
+    def insert(self, category):
+        rowid = self.db.insert('category', data={'name': category.name})
+        category.category_id = rowid
+
+
+    def get(self, category_id):
+        record = self.db.read('category', where={'id': category_id})
+
+        if not record:
+            raise CategoryNotFound()
+
+        record = record[0]
+        result = Category(record[0], record[1])
+        return result
+
+
+    def delete(self, category):
+        self.db.delete('category', where={'id': category.category_id})
+
+
+    def update(self, category):
+        self.db.update('category', data={'name': category.name}, where={'id': category.category_id})
---
+++ apollo/log.py
@@ -0,0 +1,57 @@
+from enum import Enum
+
+import datetime
+
+class Action(Enum):
+    PAUSE, FINISH, ARCHIVE, WORK, CREATE, DELETE = range(6)
+
+
+class Log(object):
+    def __init__(self, action, problem_id, name, category_id, dt=None):
+        self.action = action
+        self.problem_id = problem_id
+        self.name = name
+        self.category_id = category_id
+
+        if dt:
+            self.datetime = dt
+        else:
+            self.datetime = datetime.datetime.now()
+
+
+class LogManager(object):
+    def __init__(self, db):
+        self.db = db
+
+
+    def tolog(self, record):
+        action = Action(int(record[0]))
+        problem_id, name, category_id = record[1].split('|')
+        dt = datetime.datetime.strptime(record[2], '%Y-%m-%dT%H:%M:%S.%f')
+
+        result = Log(action, problem_id, name, category_id, dt)
+        return result
+
+    def torecord(self, log):
+        action = log.action.value
+        problem = '|'.join([str(log.problem_id), log.name, str(log.category_id)])
+        dt = log.datetime.isoformat()
+
+        return {'action': action, 'problem': problem, 'datetime': dt}
+
+    def read(self):
+        records = self.db.read('log')
+        result = []
+
+        for record in records:
+            result.append(self.tolog(record))
+
+        return result
+
+
+    def insert(self, log):
+        self.db.insert('log', self.torecord(log))
+
+
+    def dump(self):
+        self.db.delete('log')
